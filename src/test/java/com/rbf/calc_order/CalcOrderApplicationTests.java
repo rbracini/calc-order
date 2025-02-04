9cb25873c30031f91b807b2fd7355e5d80552f62
Regis Bracini <regis.bracini@gmail.com>
2025-02-03 22:13:36 -0300

    Iniciando o projeto

---
+++ src/test/java/com/rbf/calc_order/CalcOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbf.calc_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
